be0b855ab7858f32c18c2ce029d693e2e89d9ac1
xingzhao <xingzhao.vanxum.com>
2017-12-27 11:47:20 +0800

    use pwd_hash to login

--- app/auth/views.py
+++ app/auth/views.py
@@ -6,7 +6,7 @@ from app import db
 from datetime import datetime
 from app.models import User, TransferInfo
 from flask_login import login_user, login_required, current_user
-
+from werkzeug.security import generate_password_hash, check_password_hash
 
 @auth.route('/', methods=['GET', 'POST'])
 @auth.route('/login', methods=['GET', 'POST'])
@@ -14,10 +14,11 @@ def login():
     form = LoginForm()
     if form.validate_on_submit():
         user = User.query.filter_by(email=form.email.data).first()
-        if user is not None and user.password_hash == form.password.data:
+        if user is not None and user.verify_password(form.password.data):
             login_user(user, form.remember_me.data)
             return redirect(url_for('main.index'))
-        flash('Invalid username or password')
+        else:
+            flash('Invalid username or password')
     return render_template('auth/login.html', form=form)
 
 
